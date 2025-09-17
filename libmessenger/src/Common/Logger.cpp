e7add17b8376a55d7bafae167e2ba883530f6507
Kacperfis <42153314+Kacperfis@users.noreply.github.com>
2025-09-17 21:44:04 +0200

    small fix in Logger.cpp

--- libmessenger/src/Common/Logger.cpp
+++ libmessenger/src/Common/Logger.cpp
@@ -23,6 +23,6 @@ void Logger::log(const Severity severity, const std::string& message)
     auto currentTime = std::chrono::system_clock::now();
     auto timePoint = std::chrono::system_clock::to_time_t(currentTime);
     std::stringstream ss;
-    ss << std::put_time(std::localtime(&timePoint), "%Y-%m-%d %H:%M:%S") << " [" << severityVec_.at(static_cast<int>(severity)) << "] " << prefix_ << ": " << message << std::endl;
+    ss << "[" << severityVec_.at(static_cast<int>(severity)) << "] " << prefix_ << ": " << message << std::endl;
     file_ << ss.str();
 }
